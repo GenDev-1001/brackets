ca4b6c0c8d9db0a5d95979440e991886fdd5cabb
GenDev-1001 <evgenii.ubileii@bk.ru>
2022-01-20 16:10:03 +0300

    feat: realize function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const arrayStr = str.split('');
+
+    for (let i = 0; i !== arrayStr.length; i++) {
+        if (arrayStr[i] === bracketsConfig[0][0] && arrayStr[i + 1] === bracketsConfig[0][1] ||
+            arrayStr[i] === '[' && arrayStr[i + 1] === ']' ||
+            arrayStr[i] === '{' && arrayStr[i + 1] === '}' ||
+            arrayStr[i] === '|' && arrayStr[i + 1] === '|' ||
+            arrayStr[i] === '1' && arrayStr[i + 1] === '2' ||
+            arrayStr[i] === '3' && arrayStr[i + 1] === '4' ||
+            arrayStr[i] === '5' && arrayStr[i + 1] === '6' ||
+            arrayStr[i] === '7' && arrayStr[i + 1] === '7' ||
+            arrayStr[i] === '8' && arrayStr[i + 1] === '8') {
+            arrayStr.splice(i, 2);
+            i = -1;
+        }
+    }
+    return !arrayStr.length >= 1;
 }
